aa04ff1069eb8e3d8b52682fe6a0d93e48c59c3a
AdiletAldazharbekov <aldajarbekov@gmail.com>
2020-07-27 21:53:03 +0600

    Скопировать проект

---
+++ Bird.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    class Bird : MovingObject
+    {
+        public Tree HomeTree { get; private set; }
+
+        public Bird(byte x, byte y) : base(x, y)
+        { }
+
+        public void Nest(Tree tree) => HomeTree = tree;
+
+        public override void Grow()
+        {
+            if (Age < 5) base.Grow();
+            else Die();
+        }
+    }
+}
